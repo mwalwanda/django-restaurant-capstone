93fb98c96705ce90ef81bb2174d15503f8f743c0
amwalwanda <alickgitprojects@gmail.com>
2025-04-20 19:22:44 +0200

    Test API

--- LilttleLemon/Restaurant/models.py
+++ LilttleLemon/Restaurant/models.py
@@ -6,10 +6,9 @@ class Menu(models.Model):
     title = models.CharField(max_length=100, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     inventory = models.IntegerField(default=0)
-    
-       
+           
     def __str__(self):
-        return self.title
+         return f'{self.title} : {str(self.price)}'
      
 class Booking(models.Model):
     #id = models.IntegerField(primary_key=2)
